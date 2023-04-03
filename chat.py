4e24b1eeb0d25771096b501d35a5da03525c883c
liulyan <yan.b.liu@oracle.com>
2023-04-02 20:56:10 -0400

    Integrate with chatgpt

--- chat.py
+++ chat.py
@@ -7,14 +7,21 @@ key = os.getenv("C_TOKEN")
 
 openai.api_key = key
 
-response = openai.ChatCompletion.create(
-  model="gpt-3.5-turbo",
-  messages=[
+
+messages=[
         {"role": "system", "content": "You are a helpful assistant."},
         {"role": "user", "content": "Can you help me with some middle school homework question?"},
-        {"role": "assistant", "content": "Sure. I would like!"},
-        {"role": "user", "content": "How DNA works?"}        
+        {"role": "assistant", "content": "Sure. I would like!"},        
     ]
-)
 
-print(response.choices[0].message.content)
+def chat(inp):
+  messages.append({"role": "user", "content": inp})
+  response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=messages,
+  )
+  messages.append(response.choices[0].message)
+  return response.choices[0].message.content
+
+print(chat("What's the meaning of life?"))
+print(chat("What's the meaning of your life?"))
